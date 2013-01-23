5d7d10d5d934f514863e75e3279d5032f6c11b97
Max Bronsema <Max.Bronsema@wwu.edu>
2013-01-23 14:30:30 -0800

    WWUZEN-1 #comment Search box now operates in a fashion compatible with smaller screens.

--- template.php
+++ template.php
@@ -138,6 +138,29 @@ function wwuzen_preprocess_html(&$variables, $hook) {
 }
 // */
 
+
+//Modify the default search block form. 
+function wwuzen_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    $form['search_block_form']['#size'] = 20;  // define size of the textfield
+    $form['search_block_form']['#default_value'] = t(''); // Set a default value for the textfield
+    $form['actions']['submit']['#value'] = t('Search'); // Change the text on the submit button
+    //$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
+
+    // Add extra attributes to the text box
+    //form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search this site.';}";
+    //$form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
+    // Prevent user from searching the default text
+    $form['#attributes']['onsubmit'] = "if(this.search_block_form.value==''){ alert('Please enter a term or terms to search for.'); return false; }";
+
+    // Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search this site.');
+  }
+} 
+
+
 /**
  * Override or insert variables into the page templates.
  *
@@ -149,10 +172,12 @@ function wwuzen_preprocess_html(&$variables, $hook) {
 
 function wwuzen_preprocess_page(&$variables, $hook) {
   //Render the search box variable so we can place it in the header.
-  $search_box = drupal_get_form('search_form');
+  $search_box = drupal_get_form('search_block_form');
   $variables['search_box'] = drupal_render($search_box);
 }
 // */
+
+
 
 /**
  * Override or insert variables into the node templates.
