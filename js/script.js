af7365c2bee6b6da901ac26a3c5e1c461d7e8142
Amy Brown <amy@rainycats.com>
2013-05-07 09:33:06 -0700

    Commit

--- js/script.js
+++ js/script.js
@@ -28,6 +28,13 @@ Drupal.behaviors.siteNameTypography = {
 	}
 }
 
+Drupal.behaviors.uiIconStuff = {
+		attach: function () {
+
+		$('.accordion-link').wrapInner('<a href="http://google.com" />');
+	}
+}
+
 	Drupal.behaviors.searchSlider = {
 		attach: function () {
 
