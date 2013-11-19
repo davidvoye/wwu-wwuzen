a1f8937cdfa1173105c57386473fb9b2862ef9ae
Max Bronsema <Max.Bronsema@wwu.edu>
2013-11-19 08:32:04 -0800

    Removed some old js code.

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ Drupal.behaviors.siteNameTypography = {
 		var siteName = $(".site-name p").text();
 
 		var typographyAnd = siteName.replace("and", "<span class=\"diminutive-type\">and</span>");
-		
+
 		var typographyCollegeOf = typographyAnd.replace("College of", "<span class=\"diminutive-type\">College of</span>");
 		var typographyDepartmentOf = typographyAnd.replace("Department of", "<span class=\"diminutive-type\">Department of</span>");
 		var typographyDivisionOf = typographyAnd.replace("Division of", "<span class=\"diminutive-type\">Division of</span>");
@@ -79,7 +79,7 @@ Drupal.behaviors.siteNameTypography = {
 
 			$('.mobile-main-nav').click(function () {
 				var mainMenu = $('.main-nav .menu');
-				
+
 				if (mainMenu.is(':visible')) {
 					mainMenu.slideToggle();
 				} else {
@@ -90,7 +90,7 @@ Drupal.behaviors.siteNameTypography = {
 					});
 				}
 			});
-			
+
 		}
 	}
 	//END MAIN MENU TOGGLE CODE
@@ -129,7 +129,7 @@ Drupal.behaviors.siteNameTypography = {
 			var mainMenu = $('.main-nav div > .menu');
 			var subMenus = $('.main-nav .menu .menu');
 			var subMenuItems = $('.main-nav .menu .menu li');
-			
+
 			$(window).resize(function() {
 				if ($(window).width() > 800) {
 					// Reset the display property for the main menu, child menus, and child menu items.
@@ -140,64 +140,10 @@ Drupal.behaviors.siteNameTypography = {
 					$('.main-nav li').removeClass("opened");
 				}
 			});
-			
+
 		}
 	}
 	// END MENU TOGGLE ON RESIZE EVENT
-
-//These next two behaviors do the exact same thing, on onLoad and one on resize
-
-/*Drupal.behaviors.resizeHeaderOnLoad = {
-  attach: function () {
-  //START RESIZE COLLEGE HEADER AND SET HEIGHT CODE
-  $(window).load(function () {
-    var divHeight = $(".site-header").children().outerHeight(true);
-    var menuHeight = $(".main-nav").outerHeight(true);
-    //For the mobile views we need another container to use since main menu is collapsed
-    var nameAndSlogan = $(".site-name").outerHeight(true);
-    //add the two heights and 10 more pixels to compensate for space beneath the main menu
-    var totalHeightWithMenu = divHeight + menuHeight + 10;
-    var totalHeightSansMenu = divHeight + nameAndSlogan;
-    //Testing for a condition forces the height to be calculated.
-      if (totalHeightSansMenu <= 61) {
-        totalHeightSansMenu = totalHeightSansMenu + 76;
-        $('.site-header').css({'height': totalHeightSansMenu + "px"});
-      } else if (totalHeightSansMenu <= 84) {
-        totalHeightSansMenu = totalHeightSansMenu + 1;
-        $('.site-header').css({'height': totalHeightSansMenu + "px"});
-        } else {
-          $('.site-header').css({'height': totalHeightWithMenu + "px"});
-          }
-    });
-    //END RESIZE COLLEGE HEADER CODE
-  }
-}
-
-Drupal.behaviors.resizeHeaderOnResize = {
-  attach: function () {
-  //START RESIZE COLLEGE HEADER AND SET HEIGHT CODE
-  $(window).resize(function () {
-    var divHeight = $(".site-header").children().outerHeight(true);
-    var menuHeight = $(".main-nav").outerHeight(true);
-    //For the mobile views we need another container to use since main menu is collapsed
-    var nameAndSlogan = $(".site-name").outerHeight(true);
-    //add the two heights and 10 more pixels to compensate for space beneath the main menu
-    var totalHeightWithMenu = divHeight + menuHeight + 10;
-    var totalHeightSansMenu = divHeight + nameAndSlogan;
-    //Testing for a condition forces the height to be calculated.
-      if (totalHeightSansMenu <= 61) {
-        totalHeightSansMenu = totalHeightSansMenu + 76;
-        $('.site-header').css({'height': totalHeightSansMenu + "px"});
-      } else if (totalHeightSansMenu <= 84) {
-        totalHeightSansMenu = totalHeightSansMenu + 1;
-        $('.site-header').css({'height': totalHeightSansMenu + "px"});
-        } else {
-          $('.site-header').css({'height': totalHeightWithMenu + "px"});
-          }
-    });
-    //END RESIZE COLLEGE HEADER CODE
-  }
-}*/
 
 
 // Tooltips for the staff and faculty directories
