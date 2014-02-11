fce508c4a71531a42507a2f1de375f785949761e
root <root@ubuntu.(none)>
2014-02-11 11:58:00 -0800

    Clean CFPA panel and css complete.

--- layouts/clean-cfpa/clean-cfpa.tpl.php
+++ layouts/clean-cfpa/clean-cfpa.tpl.php
@@ -1,15 +1,15 @@
 <div class="clean-cfpa cfpa-top clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 	<?php print $content['top']; ?>
 </div>
-<div class="clean-cfpa cfpa-bottom clearfix">
-	<div class="clean-cfpa cfpa-bottom-left">
+<div class="clean-cfpa cfpa-bottom clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+	<div class="clean-cfpa cfpa-bottom-left" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 		<?php print $content['bottom-left']; ?>
 	</div>
-	<div class="clean-cfpa cfpa-bottom-right">
-		<div class="clean-cfpa cfpa-bottom-right-top">
+	<div class="clean-cfpa cfpa-bottom-right" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+		<div class="clean-cfpa cfpa-bottom-right-top" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 			<?php print $content['bottom-right-top']; ?>
 		</div>
-		<div class="clean-cfpa cfpa-bottom-right-bottom">
+		<div class="clean-cfpa cfpa-bottom-right-bottom" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 			<?php print $content['bottom-right-bottom']; ?>
 		</div>
 	</div>
