6e9d96a05f4707ccfa90c7a0995c163fc1d8f4c5
Nigel Packer <nigel.packer@wwu.edu>
2014-02-18 10:16:43 -0800

    Refactored classes for better css structure.

--- layouts/clean-2x2/clean-2x2.tpl.php
+++ layouts/clean-2x2/clean-2x2.tpl.php
@@ -9,19 +9,19 @@
  */
 ?>
 
-<div class="clean-2x2 2x2-top">
-	<div class="clean-2x2 2x2-top-left">
+<div class="clean-2x2 clean-2x2-top clean-2x2-container">
+	<div class="clean-2x2 clean-2x2-top clean-2x2-left">
 		<?php echo $content['top-left']; ?>
 	</div>
-	<div class="clean-2x2 2x2-top-right">
+	<div class="clean-2x2 2x2-top clean-2x2-right">
 		<?php echo $content['top-right']; ?>
 	</div>
 </div>
-<div class="clean-2x2 2x2-bottom">
-	<div class="clean-2x2 2x2-bottom-left">
+<div class="clean-2x2 clean-2x2-bottom clean-2x2-container">
+	<div class="clean-2x2 clean-2x2-bottom clean-2x2-left">
 		<?php echo $content['bottom-left']; ?>
 	</div>
-	<div class="clean-2x2 2x2-bottom-right">
+	<div class="clean-2x2 2x2-bottom clean-2x2-right">
 		<?php echo $content['bottom-right']; ?>
 	</div>
 </div>
