a4c6986d1ed494719552c9831bf2818d0a455c17
Nigel Packer <nigel.packer@wwu.edu>
2014-12-01 12:58:13 -0800

    image caption wrapper fix

--- js/script.js
+++ js/script.js
@@ -213,23 +213,43 @@
     attach: function (context, settings) {
       $('.flex-caption', context).each(function () {
         var containerHeight,
-      height,
-      imageHeight,
-      topMargin;
+            height,
+            imageHeight,
+            topMargin;
 
-      $this = $(this);
-      containerHeight = $this.closest('ul').height();
-      imageHeight = $this.siblings('img').height();
-      height = $this.height();
-      topMargin = (containerHeight - height - imageHeight) / 2;
+        $this = $(this);
+        containerHeight = $this.closest('ul').height();
+        imageHeight = $this.siblings('img').height();
+        height = $this.height();
+        topMargin = (containerHeight - height - imageHeight) / 2;
 
-      $this.css({
-        'position': 'relative',
-        'margin-top': topMargin
-      });
+        $this.css({
+          'position': 'relative',
+          'margin-top': topMargin
+        });
       });
     }
   }
   // END Slideshow caption
+ 
+  // START Slideshow image links
+  Drupal.behaviors.slideshowImageLinks = {
+    attach: function (context, settings) {
+      $('.slides li').each(function () {
+        var $this,
+            $link;
+
+        $this = $(this);
+        $link = $this.find('.flex-caption a');
+        
+        if ($link.length > 0) {
+          $this.find('img').wrap($('<a/>', {
+            'href': $link.attr('href')
+          }));
+        }
+      });
+    }
+  }
+  //END Slideshow image links
 
 })(jQuery, Drupal, this, this.document);
