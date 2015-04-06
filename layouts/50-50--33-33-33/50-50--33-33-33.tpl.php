595e1ffb90f7d65f1dd93514e60a201178c403c4
Max Bronsema <Max.Bronsema@wwu.edu>
2015-04-06 16:05:48 -0700

    Made the class names not start with a numeral.

--- layouts/50-50--33-33-33/50-50--33-33-33.tpl.php
+++ layouts/50-50--33-33-33/50-50--33-33-33.tpl.php
@@ -12,23 +12,23 @@
  *   panel of the layout.
  */
 ?>
-<div class="50-50--33-33-33 50-50--33-33-33-container clearfix">
-  <div class="50-50--33-33-33 50-50--33-33-33-top-row clearfix">
-    <div class="50-50--33-33-33 50-50--33-33-33-top-left clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+<div class="fifty-50--33-33-33 fifty-50--33-33-33-container clearfix">
+  <div class="fifty-50--33-33-33 fifty-50--33-33-33-top-row clearfix">
+    <div class="fifty-50--33-33-33 fifty-50--33-33-33-top-left clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
       <?php print $content['top_left']; ?>
     </div>
-    <div class="50-50--33-33-33 50-50--33-33-33-top-right clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+    <div class="fifty-50--33-33-33 fifty-50--33-33-33-top-right clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
       <?php print $content['top_right']; ?>
     </div>
   </div>
-  <div class="50-50--33-33-33 50-50--33-33-33-bottom-row clearfix">
-    <div class="50-50--33-33-33 50-50--33-33-33-bottom-left clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+  <div class="fifty-50--33-33-33 fifty-50--33-33-33-bottom-row clearfix">
+    <div class="fifty-50--33-33-33 fifty-50--33-33-33-bottom-left clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
       <?php print $content['bottom_left']; ?>
     </div>
-    <div class="50-50--33-33-33 50-50--33-33-33-bottom-center clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+    <div class="fifty-50--33-33-33 fifty-50--33-33-33-bottom-center clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
       <?php print $content['bottom_center']; ?>
     </div>
-    <div class="50-50--33-33-33 50-50--33-33-33-bottom-right clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+    <div class="fifty-50--33-33-33 fifty-50--33-33-33-bottom-right clearfix" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
       <?php print $content['bottom_right']; ?>
     </div>
   </div>
