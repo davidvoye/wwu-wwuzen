1db1f5a0dcbd5cb9fd4c9e2b9abae602c70339e7
Amerie Lommen <lommena@wwu.edu>
2016-03-14 11:26:44 -0700

    issue #CDS-1027 #comment Added 70-30 40-40-20 panel layout for geology homepage.

---
+++ layouts/clean-70-30-40-40-20/clean-70-30-40-40-20.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Template for CFPA department landing page panels
+ */
+?>
+<div class="clean-70-30-40-40-20-container">
+  <div class="clean-70-30-40-40-20-top-row">
+    <div class="clean-70-30-40-40-20-top-left" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+      <?php print $content['top_left']; ?>
+    </div>
+    <div class="clean-70-30-40-40-20-top-right" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+      <?php print $content['top_right']; ?>
+    </div>
+  </div>
+  <div class="clean-70-30-40-40-20-bottom-row">
+    <div class="clean-70-30-40-40-20-bottom-left" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+      <?php print $content['bottom_left']; ?>
+    </div>
+    <div class="clean-70-30-40-40-20-bottom-middle" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+      <?php print $content['bottom_middle']; ?>
+    </div>
+    <div class="clean-70-30-40-40-20-bottom-right" <?php if (!empty($css_id)) {print "id=\"$css_id\""; } ?>>
+      <?php print $content['bottom_right']; ?>
+    </div>
+  </div>
+</div>
+
